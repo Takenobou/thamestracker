936a5f9d6b2f4420effdc5d692272776b3f5ce35
Alex Martin <aam65@student.le.ac.uk>
2025-03-17 12:27:43 +0000

    Added signal handling in the server main file to intercept SIGINT and SIGTERM for graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Takenobou/thamestracker/config"
 	"github.com/Takenobou/thamestracker/internal/api"
@@ -28,6 +32,22 @@ func main() {
 
 	serverAddr := fmt.Sprintf(":%d", config.AppConfig.Server.Port)
 	logger.Logger.Infof("Server running, address: %s", serverAddr)
+
+	shutdownCh := make(chan os.Signal, 1)
+	signal.Notify(shutdownCh, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-shutdownCh
+		logger.Logger.Infof("Shutdown signal received, shutting down gracefully...")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := app.Shutdown(); err != nil {
+			logger.Logger.Errorf("Error during server shutdown: %v", err)
+		}
+		<-ctx.Done()
+		logger.Logger.Infof("Server has been shut down.")
+		os.Exit(0)
+	}()
 
 	if err := app.Listen(serverAddr); err != nil {
 		logger.Logger.Errorf("Failed to start server: %v", err)
